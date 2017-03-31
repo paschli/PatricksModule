b56b3f6889df11dd0fde97a7f7c33b41773ce0ed
paschli <patrick@schlischka.de>
2017-03-31 23:49:15 +0200

    module.json aktualisiert

--- LCN_LA/module.php
+++ LCN_LA/module.php
@@ -14,7 +14,7 @@ class LCNLA extends IPSModule {
     $status=$this->RegisterPropertyBoolean('Status', FALSE);
     $this->RegisterPropertyInteger('idLCNInstance', 0); //Id der zu beobachtenden Variable
     $this->RegisterPropertyInteger('LaempchenNr', 0);	
-    $statusID = $this->RegisterVariableBoolean('Status','Status','~switch');//
+    $statusID = $this->RegisterVariableBoolean('Status','Status','~Switch');//
     IPS_SetIcon($this->GetIDForIdent('Status'), 'Bulb');
 
     
