30ee74d177ffa2dd5715674f145d601478af5578
paschli <patrick@schlischka.de>
2017-04-01 00:31:58 +0200

    module.json aktualisiert

--- LCN_LA/module.php
+++ LCN_LA/module.php
@@ -41,7 +41,7 @@ class LCNLA extends IPSModule {
     IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
     if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
   }*/
- public function RequestAction($Status) {
+ public function RequestAction($Status, $value) {
  
     if($status){
         LCN_SetLamp($lcn_instID,$lampNo,'E');  
