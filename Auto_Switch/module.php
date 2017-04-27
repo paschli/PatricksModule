73df9bddc3cb129cb1660f4cedb796caf0240316
schlis <schlis@W5DEVELC0402.emea.vorwerk.org>
2017-04-27 17:45:23 +0200

    Changes 1

--- Auto_Switch/module.php
+++ Auto_Switch/module.php
@@ -25,10 +25,6 @@ class AutSw extends IPSModule {
     parent::ApplyChanges();
     $statusID = $this->RegisterVariableBoolean('Status','Status','~Switch');//
     $this->RegisterPropertyBoolean('Status', FALSE);
-    $autoffID=$this->RegisterVariableBoolean('AutoOff','Auto Off','~Switch');//
-    $autoff=$this->RegisterPropertyBoolean('AutoOff', FALSE);
-    $timerID=$this->RegisterVariableBoolean('Timer','Timer','~Switch');//
-    $autoff=$this->RegisterPropertyBoolean('Timer', FALSE);
     $this->RegisterPropertyInteger('Auswahl', 0); //Id der zu beobachtenden Variable
     $this->RegisterPropertyInteger('idLCNInstance', 0);
     $this->RegisterPropertyInteger('LaempchenNr', 0);
@@ -66,6 +62,10 @@ class AutSw extends IPSModule {
         IPS_SetPosition($VarID, 10);
         IPS_SetIcon($VarID, 'Hourglass');
         IPS_SetParent($VarID,$instID );
+        $autoffID=$this->RegisterVariableBoolean('AutoOff','Auto Off','~Switch');//
+        $autoff=$this->RegisterPropertyBoolean('AutoOff', FALSE);
+        $timerID=$this->RegisterVariableBoolean('Timer','Timer','~Switch');//
+        $autoff=$this->RegisterPropertyBoolean('Timer', FALSE);
         IPS_SetParent($autoffID,$CatID );
         IPS_SetParent($timerID,$CatID );
     }
