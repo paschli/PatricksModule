e92d2bccca1a9ad7d00ea99fe2f058451e4bbad6
schlis <schlis@W5DEVELC0402.emea.vorwerk.org>
2017-05-09 15:21:48 +0200

    Changes 1

--- Auto_Switch/module.php
+++ Auto_Switch/module.php
@@ -209,12 +209,11 @@ class AutSw extends IPSModule {
 
 public function EventTrigger(int $par,bool $value) {
     IPS_LogMessage("AutoSwitch_EventTrigger","Ident: ".$par." Value: ".$value);
-    
+    $CatID =IPS_GetCategoryIDByName('Konfig', $par);
+    $AutoOffID=IPS_GetObjectIDByIdent('AutoOff_Switch', $CatID);
     if($value && GetValueBoolean($AutoOffID)){
-        $CatID =IPS_GetCategoryIDByName('Konfig', $par);
         $LaufzeitID= IPS_GetVariableIDByName('Set Laufzeit', $CatID);
         $Laufzeit= GetValueInteger($LaufzeitID);
-        $AutoOffID=IPS_GetObjectIDByIdent('AutoOff_Switch', $CatID);
         $IDLaufz= IPS_GetVariableIDByName('Laufzeit', $par);
         $TimerID=@$this->GetIDForIdent('AutoOffTimer');
         if($TimerID)
