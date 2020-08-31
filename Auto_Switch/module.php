59f6c83c893e48c44d3945bfe0c9830b2820c2d6
Patrick Schlischka <patrick@schlischka.de>
2020-08-31 15:37:00 +0200

    MQTT TimeOut für Erfolgskontrolle geändert

--- Auto_Switch/module.php
+++ Auto_Switch/module.php
@@ -902,7 +902,7 @@ private function Set_MQTT($value) {
             default: $commandValue="OFF";
         }
         RequestAction($SetID, $commandValue);
-        usleep(100000);
+        sleep(500000);
         $StatusValue=GetValueString(IPS_GetChildrenIDs($StatusID)[0]);
         if($StatusValue==$commandValue){
             SetValue($this->GetIDForIdent("Status"), $value);
