9db7e44628bcff579fc6aaa0e6c4d617f6e92fc7
Patrick Schlischka <patrick@schlischka.de>
2023-05-03 10:35:41 +0200

    logs

--- Auto_Switch/module.php
+++ Auto_Switch/module.php
@@ -1036,9 +1036,10 @@ private function CreateWahlVar($ident,$name,$icon,$par, $pos){
 
 private function FindTargetStatusofDevices($type) {
     
-// ID der zu steuernden Instanz ermitteln    
+// ID der zu steuernden Instanz ermitteln
+    $ReferenceList = $this->GetReferenceList();
     $ZielID= $this->ReadPropertyInteger('idLCNInstance');
-    IPS_LogMessage("(".$this->$_IPS['SELF'].")AutoSwitch_FindTargetStatusofDevices","Suche Id vom Typ= ".$type." bei ID=".$ZielID);
+    IPS_LogMessage("(".$ReferenceList.")AutoSwitch_FindTargetStatusofDevices","Suche Id vom Typ= ".$type." bei ID=".$ZielID);
 //Children dieser Instanz ermitteln    
     $ID_Children=IPS_GetChildrenIds($ZielID);
     switch($type){
