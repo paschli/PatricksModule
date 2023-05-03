18da55f2e3df7a09147be0a880ea1b7069470718
Patrick Schlischka <patrick@schlischka.de>
2023-05-03 10:18:35 +0200

    logs

--- Auto_Switch/module.php
+++ Auto_Switch/module.php
@@ -1038,7 +1038,7 @@ private function FindTargetStatusofDevices($type) {
     
 // ID der zu steuernden Instanz ermitteln    
     $ZielID= $this->ReadPropertyInteger('idLCNInstance');
-    IPS_LogMessage("(".$this->$_IPS['Self'].")AutoSwitch_FindTargetStatusofDevices","Suche Id vom Typ= ".$type." bei ID=".$ZielID);
+    IPS_LogMessage("(".$_IPS['SELF'].")AutoSwitch_FindTargetStatusofDevices","Suche Id vom Typ= ".$type." bei ID=".$ZielID);
 //Children dieser Instanz ermitteln    
     $ID_Children=IPS_GetChildrenIds($ZielID);
     switch($type){
