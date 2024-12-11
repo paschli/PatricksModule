1eec360a174c1b36e3eea4c6963bcdd0a3dcd356
Patrick Schlischka <patrick@schlischka.de>
2024-12-11 11:32:36 +0100

    Bug Fix

--- Auto_Switch_2/module.php
+++ Auto_Switch_2/module.php
@@ -1071,7 +1071,7 @@ private function CreateTimerVar($ident,$name,$CatID,$pos,$icon,$script,$profil){
     IPS_SetEventTrigger($watchEventID, 1, $wertID);        //Bei Änderung von Variable mit ID 15754
     IPS_SetParent($watchEventID, $VarID);                  //Ereignis zuordnen
     //IPS_SetEventAction($watchEventID, $SkriptID, []);      // Ziel von Event auf control setzen
-    IPS_SetEventAction($EreignisID, "{7938A5A2-0981-5FE0-BE6C-8AA610D654EB}", []);
+    IPS_SetEventAction($watchEventID, "{7938A5A2-0981-5FE0-BE6C-8AA610D654EB}", []);
     IPS_SetEventActive($watchEventID, true);               //Ereignis aktivieren
     
     
