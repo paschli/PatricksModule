a9f91b890575ee03aa141e6b24532b4d81d1ba31
Patrick Schlischka <patrick@schlischka.de>
2024-12-11 11:00:34 +0100

    Bug Fix

--- Auto_Switch_2/module.php
+++ Auto_Switch_2/module.php
@@ -1064,7 +1064,7 @@ private function CreateTimerVar($ident,$name,$CatID,$pos,$icon,$script,$profil){
     $eventID=$this->CreateTimeEvent($name.'Event', $VarID, 1, $eventScript);
     
     $wertID=$this->CreateAnzVar($name.'Wert',$name.'-Zeit',$CatID,$pos+1,'Clock','','~UnixTimestampTime');
-    $this->CreateScriptForVar($wertID,"<? SetValue(\$_IPS['VARIABLE'], \$_IPS['VALUE']);");
+    $this->CreateScriptForVar($wertID,"<? SetValue(\$_IPS['VARIABLE'], \$_IPS['VALUE']);",'control');
     IPS_SetHidden ($wertID, TRUE);
     
     $watchEventID=IPS_CreateEvent(0);                      //ausgelöstes Ereignis bei Änderung der Zeit
